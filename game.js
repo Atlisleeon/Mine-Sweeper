0784c4ce8412cef6d28c613e2f17efefc091f096
Lee-On Atlis <32301391+Atlisleeon@users.noreply.github.com>
2023-08-03 21:18:14 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const MINE = '💥'
 const FLAGES = '🇮🇱'
 const NUMBERS = ('12345678')
 
-// THE MODEL
+// THE MODEL (database) >>> into the cells
 var gBoard = {
     mineAroundCount: 4,
     isShown: false,
@@ -26,7 +26,32 @@ var gLevel = {
         SIZE: [12, 12],
         MINES: 32,
     }
+
 }
+
+// function selectLevel(level) {
+//     switch (level) {
+//         case 'beginner':
+//             numRow = 4;
+//             numCol = 4;
+//             MINES = 2;
+//             break;
+//         case 'medium':
+//             numRow = 8;
+//             numCol = 8;
+//             MINES = 14,
+//         break
+//         case 'expert':
+//             numRow = 12;
+//             numCol = 12;
+//             MINES = 32,
+//         break
+//         default:
+//             numRow = 4;
+//             numCol = 4;
+//             MINES = 2;
+//     }
+// }
 
 var gGame = {
     isOn: false,
@@ -41,17 +66,21 @@ function onInit() {
     gBoard = buildBoard()
     renderBoard(gBoard)
     gGame.isOn = true
+    // setMines(gBoard)
 
     // console.table(gBoard)
     // console.log(gBoard)
 }
 
 function renderBoard(board) {
+    console.log(board)
     var strHTML = ''
     for (var i = 0; i < board.length; i++) {
         strHTML += `<tr>\n`
         for (var j = 0; j < board[i].length; j++) {
-            strHTML += `<td>${board[i][j]}</td>\n`
+            if (board[i][j].isMine) strHTML += '<td>' + MINE + '</td>'
+            else strHTML += '<td>' + '</td>'
+            // strHTML += `<td>${board[i][j]}</td>\n`
         }
         strHTML += `</tr>\n`
     }
@@ -59,25 +88,62 @@ function renderBoard(board) {
     const elTable = document.querySelector('.board')
     elTable.innerHTML = strHTML
     // console.table(board)
+
 }
 
 
+// function buildBoard() {
+//     const level = 4
+//     const board = []
 
+//     // replace(add) the size with the levels
+//     for (var i = 0; i < level; i++) {
+//         board.push([])
+
+//         for (var j = 0; j < level; j++) {
+//             board[i][j] = (Math.random() > 0.8) ? MINE : ''
+//             // board[1][2] = 'MINE'
+//             // console.log(board)
+//         }
+//     }
+//     return board;
+// }
 
 function buildBoard() {
-    var board = []
-    // replace(add) the size with the levels
+    // const level = 4
+    const board = []
+
     for (var i = 0; i < 4; i++) {
         board.push([])
 
         for (var j = 0; j < 4; j++) {
-            board[i][j] = (Math.random() > 0.8) ? MINE : ''
-            // board[1][2] = 'MINE'
-            // console.log(board)
+            board[i][j] = {
+                mineAroundCount: 4,
+                isShown: false,
+                isMine: (Math.random() > 0.8) ? MINE : '',
+                isMarked: false,
+
+                // board[1][2] = 'MINE'
+                // console.log(board)
+            }
         }
     }
+    console.log(board)
     return board;
 }
+
+// function setMines(board) {
+//     const MINE = {
+//         location: {
+//             i: 1,
+//             j: 2
+//         },
+//         // currCellContent: '',
+//     }
+//     MINE.push(MINE)
+//     board[MINE.location.i][MINE.location.j] = MINE
+// }
+
 
 // function buildBoard() {
 
@@ -111,11 +177,10 @@ function onOpenModal() {
     onCloseModal()
 }
 
-
 function onGameOver() {
     const elModalText = document.querySelector('.modal h2')
-    elModalText.innerText = `You were blessed at current - time ${getTime()}`
-    onOpenModal()
+    // elModalText.innerText = `You were blessed at current - time ${getTime()}`
+    // onOpenModal()
 }
 
 function onCloseModal() {
@@ -153,3 +218,14 @@ console.log(mines); // Output: 2
 // if (gBoard[rowIdx][colIdx] === MINE) {
 //     gBoard[rowIdx][colIdx] = gameOver
 // }
+
+
+
+var res = getRandomIntInclusive(0, gBoard.length)
+console.log(res)
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//getEmptyCell --- pacman... cr...
